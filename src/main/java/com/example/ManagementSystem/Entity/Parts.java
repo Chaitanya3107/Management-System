aa9d25e029037759b4d0d911048cad3b974159bd
Chaitanya_Anchal <chaitanyaanchal3107@gmail.com>
2026-01-04 20:49:19 +0530

    started working on schema of project, created parts entity to store parts

---
+++ src/main/java/com/example/ManagementSystem/Entity/Parts.java
@@ -0,0 +1,31 @@
+package com.example.ManagementSystem.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "parts")
+public class Parts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String brand;
+
+    @Column(nullable = false)
+    private BigDecimal sellingPrice;
+
+    @Column(nullable = false)
+    private Integer stockQuantity;
+
+}
